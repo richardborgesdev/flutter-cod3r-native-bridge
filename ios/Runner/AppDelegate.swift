3b85e21c1ee1eaf9e196e75f62b221aab7cf4421
Richard Borges <richard.borges.cc@gmail.com>
2023-06-07 09:48:15 -0300

    chore: ios calc sum to flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,29 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel : FlutterMethodChannel(
+        name: "cod3r.com.br/nativo",
+        binaryMessenger: FlutterBinaryMessenger,
+      )
+
+      channel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult ) -> Void in
+
+        guard call.method == "calcSum" else {
+          result(FlutterMethodNotImplemented)
+          return
+        }
+
+        if let args = call.arguments as? [String: Any],
+          let num1 = args['num1'] as? Int,
+          let num2 = args['num2'] as? Int {
+            result(a + b)
+        } else {
+          result(-1)
+        }
+      })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
